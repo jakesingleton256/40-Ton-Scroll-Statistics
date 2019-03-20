a0ab562139649a40f90ef8875e1d675cc4bc6a14
Jake Singleton <jake.singleton@okstate.edu>
2019-03-20 13:32:27 -0500

    Plot uncertainties vs other variable

--- ex2.py
+++ ex2.py
@@ -8,8 +8,16 @@ import xlrd
 from os.path import join
 from glob2 import glob
 import datetime as dt
+from matplotlib.backends.backend_pdf import PdfPages
 x,y,w,z=[[],[],[],[]]
 y=[]
+Pdis_uncert=[]
+Psuc_uncert=[]
+Tsuc_uncert=[]
+T_cond=[]
+BP=[]
+WP=[]
+Twi=[]
 writer = ExcelWriter('total uncertainty.xlsx')
 for name in glob2.glob('**/*'):
     if name.endswith((".csv")):
@@ -27,10 +35,25 @@ for name in glob2.glob('**/*'):
         Psuc = df1[u'Psuc']
         Tsuc = df1[u'Tsuc']
         Pdis = df1[u'Pdis']
-        Twi = df1[u'Twi']
+        Twi.append(df1[u'Twi'].mean())
         Twe = df1[u'Twe']
-        BP = df1[u'Bypass Position']
-        WP =  df1[u'Water Pump Speed']
+        BP.append(df1[u'Bypass Position'].mean())
+        WP.append(df1[u'Water Pump Speed'].mean())
+
+        if '70' in name:
+            T_cond.append(70)
+        elif '80' in name:
+            T_cond.append(80)
+        elif '90' in name:
+            T_cond.append(90)
+        elif '100' in name:
+            T_cond.append(100)
+        elif '110' in name:
+            T_cond.append(110)
+        elif '120' in name:
+            T_cond.append(120)
+        elif '130' in name:
+            T_cond.append(130)
 
         # Calculate averages for key variables
         Ps_avg = df1[u'Psuc'].mean()
@@ -57,7 +80,9 @@ for name in glob2.glob('**/*'):
         s_Psuc=s_x_Psuc/np.sqrt(N)
         s_Tsuc=s_x_Tsuc/np.sqrt(N)
         s_Pdis=s_x_Pdis/np.sqrt(N)
-        
+        Psuc_uncert.append(s_Psuc)
+        Tsuc_uncert.append(s_Tsuc)
+        Pdis_uncert.append(s_Pdis)
 ##        print("s_Psuc=",s_Psuc)
 ##        print("s_Tsuc=",s_Tsuc)
 ##        print("s_Pdis=",s_Pdis)
@@ -79,7 +104,156 @@ for name in glob2.glob('**/*'):
         data = {'Random':[s_Psuc, s_Tsuc, s_Pdis], 'Total':[u_Psuc, u_Tsuc, u_Pdis]}
         d = pd.DataFrame(data, index = ['Psuc', 'Tsuc', 'Pdis'], columns=['Random', 'Total'])
         d.to_excel(writer, '%s' % os.path.basename(Filename[:-4]))
+
 writer.save()
+
+with PdfPages('Condensing Temp v Pdis.pdf') as pdf:
+    plt.figure(1)
+    plt.scatter(T_cond, Pdis_uncert)
+    z = np.polyfit(T_cond, Pdis_uncert, 1)
+    p = np.poly1d(z)
+    plt.plot(T_cond, p(T_cond), "r-")
+    plt.xlabel('Condensing Temperature [F]')
+    plt.ylabel(' Discharge Pressure Random Uncertainty (psi)')
+    plt.title('Condensing Temp v Pdis Uncertainty')
+    #plt.text(0, 54.35, 'Psuc =  %s \u00B1  %s' % (round(Ps_avg, 2), round(u_Psuc, 4)))
+    pdf.savefig()
+    plt.close()
+
+    plt.figure(2)
+    plt.scatter(T_cond, Psuc_uncert)
+    z = np.polyfit(T_cond, Psuc_uncert, 1)
+    p = np.poly1d(z)
+    plt.plot(T_cond, p(T_cond), "r-")
+    plt.xlabel('Condensing Temperature [F]')
+    plt.ylabel(' Suction Pressure Random Uncertainty (psi)')
+    plt.title('Condensing Temp v Psuc Uncertainty')
+    # plt.text(0, 54.35, 'Psuc =  %s \u00B1  %s' % (round(Ps_avg, 2), round(u_Psuc, 4)))
+    pdf.savefig()
+    plt.close()
+
+    plt.figure(3)
+    plt.scatter(T_cond, Tsuc_uncert)
+    z = np.polyfit(T_cond, Tsuc_uncert, 1)
+    p = np.poly1d(z)
+    plt.plot(T_cond, p(T_cond), "r-")
+    plt.xlabel('Condensing Temperature [F]')
+    plt.ylabel(' Suction Temperature Random Uncertainty (psi)')
+    plt.title('Condensing Temp v T_suc Uncertainty')
+    # plt.text(0, 54.35, 'Psuc =  %s \u00B1  %s' % (round(Ps_avg, 2), round(u_Psuc, 4)))
+    pdf.savefig()
+    plt.close()
+
+with PdfPages('Bypass Valve Position.pdf') as pdf:
+    plt.figure(1)
+    plt.scatter(BP, Pdis_uncert)
+    z = np.polyfit(BP, Pdis_uncert, 1)
+    p = np.poly1d(z)
+    plt.plot(BP, p(BP), "r-")
+    plt.xlabel('Bypass Valve Position (%)')
+    plt.ylabel(' Discharge Pressure Random Uncertainty (psi)')
+    plt.title('Bypass Valve Position v Pdis Uncertainty')
+    #plt.text(0, 54.35, 'Psuc =  %s \u00B1  %s' % (round(Ps_avg, 2), round(u_Psuc, 4)))
+    pdf.savefig()
+    plt.close()
+
+    plt.figure(2)
+    plt.scatter(BP, Psuc_uncert)
+    z = np.polyfit(BP, Psuc_uncert, 1)
+    p = np.poly1d(z)
+    plt.plot(BP, p(BP), "r-")
+    plt.xlabel('Bypass Valve Position (%)')
+    plt.ylabel(' Suction Pressure Random Uncertainty (psi)')
+    plt.title('Bypass Valve Position v Psuc Uncertainty')
+    # plt.text(0, 54.35, 'Psuc =  %s \u00B1  %s' % (round(Ps_avg, 2), round(u_Psuc, 4)))
+    pdf.savefig()
+    plt.close()
+
+    plt.figure(3)
+    plt.scatter(BP, Tsuc_uncert)
+    z = np.polyfit(BP, Tsuc_uncert, 1)
+    p = np.poly1d(z)
+    plt.plot(BP, p(BP), "r-")
+    plt.xlabel('Bypass Valve Position (%)')
+    plt.ylabel(' Suction Temperature Random Uncertainty (psi)')
+    plt.title('Bypass Valve Position v T_suc Uncertainty')
+    # plt.text(0, 54.35, 'Psuc =  %s \u00B1  %s' % (round(Ps_avg, 2), round(u_Psuc, 4)))
+    pdf.savefig()
+    plt.close()
+
+with PdfPages('Water Pump Speed.pdf') as pdf:
+    plt.figure(1)
+    plt.scatter(WP, Pdis_uncert)
+    z = np.polyfit(WP, Pdis_uncert, 1)
+    p = np.poly1d(z)
+    plt.plot(WP, p(WP), "r-")
+    plt.xlabel('Water Pump Speed (Hz)')
+    plt.ylabel(' Discharge Pressure Random Uncertainty (psi)')
+    plt.title('Water Pump Speed v Pdis Uncertainty')
+    # plt.text(0, 54.35, 'Psuc =  %s \u00B1  %s' % (round(Ps_avg, 2), round(u_Psuc, 4)))
+    pdf.savefig()
+    plt.close()
+
+    plt.figure(2)
+    plt.scatter(WP, Psuc_uncert)
+    z = np.polyfit(WP, Psuc_uncert, 1)
+    p = np.poly1d(z)
+    plt.plot(WP, p(WP), "r-")
+    plt.xlabel('Water Pump Speed (Hz)')
+    plt.ylabel(' Suction Pressure Random Uncertainty (psi)')
+    plt.title('Water Pump Speed v Psuc Uncertainty')
+    # plt.text(0, 54.35, 'Psuc =  %s \u00B1  %s' % (round(Ps_avg, 2), round(u_Psuc, 4)))
+    pdf.savefig()
+    plt.close()
+
+    plt.figure(3)
+    plt.scatter(WP, Tsuc_uncert)
+    z = np.polyfit(WP, Tsuc_uncert, 1)
+    p = np.poly1d(z)
+    plt.plot(WP, p(WP), "r-")
+    plt.xlabel('Water Pump Speed (Hz)')
+    plt.ylabel(' Suction Temperature Random Uncertainty (psi)')
+    plt.title('Water Pump Speed v T_suc Uncertainty')
+    # plt.text(0, 54.35, 'Psuc =  %s \u00B1  %s' % (round(Ps_avg, 2), round(u_Psuc, 4)))
+    pdf.savefig()
+    plt.close()
+
+with PdfPages('Inlet Water Temperature.pdf') as pdf:
+    plt.figure(1)
+    plt.scatter(Twi, Pdis_uncert)
+    z = np.polyfit(Twi, Pdis_uncert, 1)
+    p = np.poly1d(z)
+    plt.plot(Twi, p(Twi), "r-")
+    plt.xlabel('Inlet Water Temperature (F)')
+    plt.ylabel(' Discharge Pressure Random Uncertainty (psi)')
+    plt.title('Inlet Water Temperature v Pdis Uncertainty')
+    #plt.text(0, 54.35, 'Psuc =  %s \u00B1  %s' % (round(Ps_avg, 2), round(u_Psuc, 4)))
+    pdf.savefig()
+    plt.close()
+
+    plt.figure(2)
+    plt.scatter(Twi, Psuc_uncert)
+    z = np.polyfit(Twi, Psuc_uncert, 1)
+    p = np.poly1d(z)
+    plt.plot(Twi, p(Twi), "r-")
+    plt.xlabel('Inlet Water Temperature (F)')
+    plt.ylabel(' Suction Pressure Random Uncertainty (psi)')
+    plt.title('Inlet Water Temperature v Psuc Uncertainty')
+    # plt.text(0, 54.35, 'Psuc =  %s \u00B1  %s' % (round(Ps_avg, 2), round(u_Psuc, 4)))
+    pdf.savefig()
+    plt.close()
+
+    plt.figure(3)
+    plt.scatter(Twi, Tsuc_uncert)
+    z = np.polyfit(Twi, Tsuc_uncert, 1)
+    p = np.poly1d(z)
+    plt.plot(Twi, p(Twi), "r-")
+    plt.xlabel('Inlet Water Temperature (F)')
+    plt.ylabel(' Suction Temperature Random Uncertainty (psi)')
+    plt.title('Inlet Water Temperature v T_suc Uncertainty')
+    # plt.text(0, 54.35, 'Psuc =  %s \u00B1  %s' % (round(Ps_avg, 2), round(u_Psuc, 4)))
+    pdf.savefig()
+    plt.close()
 ####            print(len(x[0]))
 ##        elif "Aplus" in name:
 ##            file2, ext2=os.path.splitext(name)
